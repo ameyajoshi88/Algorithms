6526382bf2e1f0b1cd813abbf121bb6061829408
Ameya Joshi <joshi.ameya@husky.neu.edu>
2017-02-06 23:32:31 -0500

    - added iterative approach

--- Workspace/Test/src/com/flatten/Flatten.groovy
+++ Workspace/Test/src/com/flatten/Flatten.groovy
@@ -22,38 +22,73 @@ package com.flatten
  *      b.b1: B1
  *      b.b2.b21: B21
  *  }
+ *
+ *  Both recursive, iterative solutions are provided
  */
 class Flatten {
 
-    static Map<String, String> flatten(Map<String, Object> map) {
+    static Map<String, String> flattenRecursive(Map<String, Object> map) {
         if(map == null) {
             return null
         }
         Map<String, String> flatMap = [:]
-        flatten(map, null, flatMap)
+        flattenRecursive(map, null, flatMap)
         return flatMap
     }
 
-    static void flatten(Map.Entry<String, Object> entry, String parentKey, Map<String, String> flatMap) {
+    static void flattenRecursive(Map.Entry<String, Object> entry, String parentKey, Map<String, String> flatMap) {
         String parentKeyComponent = "${parentKey != null ? parentKey + '.' : ''}"
         if(entry.value instanceof String) {
             flatMap.put("${parentKeyComponent}${entry.key}" as  String, entry.value as String)
             return
         }
-        flatten(entry.value as Map, "${parentKeyComponent}${entry.key}", flatMap)
+        flattenRecursive(entry.value as Map, "${parentKeyComponent}${entry.key}", flatMap)
     }
 
-    static void flatten(Map<String, Object> map, String parentKey, Map<String, String> flatMap) {
+    static void flattenRecursive(Map<String, Object> map, String parentKey, Map<String, String> flatMap) {
         for(Map.Entry<String, Object> entry : map.entrySet()) {
-            flatten(entry, parentKey, flatMap)
+            flattenRecursive(entry, parentKey, flatMap)
         }
+    }
+
+    static Map<String, String> flattenIterative(Map<String, Object> map) {
+        if(map == null) {
+            return null
+        }
+        Map<String, String> flatMap = [:]
+        for(Map.Entry<String, Object> entry : map.entrySet()) {
+            Set<Map.Entry<String, Object>> entries = new LinkedHashSet<>()
+            entries.add(entry)
+            Set<String> keys = []
+            while (!entries.isEmpty()) {
+                entry = entries.last() // remove recently added first
+                if(!keys.add(entry.key)) { // must be a parent whose children are already covered
+                    keys.remove(entry.key) // remove key
+                    entries.remove(entry) // remove entry
+                    continue
+                }
+                if(entry.value instanceof String) {
+                    flatMap.put("${getKey(keys)}" as String, entry.value as String)
+                    keys.remove(entry.key) // remove key
+                    entries.remove(entry) // remove this entry as it is processed completely
+                } else {
+                    Map<String, Object> valueMap = entry.value as Map
+                    entries.addAll(valueMap.entrySet())
+                }
+            }
+        }
+        return flatMap
+    }
+
+    static String getKey(Set<String> keys) {
+        return "${keys.join('.')}"
     }
 }
 
 class FlattenTester {
     static void main(String[] args) {
-        assert null == Flatten.flatten(null)
-        assert [:] == Flatten.flatten([:])
+        assert null == Flatten.flattenRecursive(null)
+        assert [:] == Flatten.flattenRecursive([:])
         Map<String, Object> map = [
                 'a' : 'A',
                 'b' : [
@@ -67,6 +102,9 @@ class FlattenTester {
                 ],
                 'c' : 'C'
         ]
-        assert ['a':'A', 'b.b1':'B1', 'b.b2.b21':'B21', 'b.b2.b22.b221':'B221', 'c':'C'] == Flatten.flatten(map)
+        assert ['a':'A', 'b.b1':'B1', 'b.b2.b21':'B21', 'b.b2.b22.b221':'B221', 'c':'C'] == Flatten.flattenRecursive(map)
+        assert null == Flatten.flattenIterative(null)
+        assert [:] == Flatten.flattenIterative([:])
+        assert ['a':'A', 'b.b1':'B1', 'b.b2.b21':'B21', 'b.b2.b22.b221':'B221', 'c':'C'] == Flatten.flattenIterative(map)
     }
 }
